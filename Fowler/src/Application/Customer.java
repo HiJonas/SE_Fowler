7df10297d9cbdc21c65b5be457fb146798f6d9c3
JoHi21 <38754316+JoHi21@users.noreply.github.com>
2019-05-02 14:05:11 +0200

    Renamed local variable each to currentRental

--- Fowler/src/Application/Customer.java
+++ Fowler/src/Application/Customer.java
@@ -24,16 +24,16 @@ public class Customer {
 
         while (enum_rentals.hasMoreElements()) {
             double thisAmount = 0;
-            Rental each = enum_rentals.nextElement();
+            Rental currentRental = enum_rentals.nextElement();
             //determine amounts for each line
-            thisAmount = each.amountFor();
+            thisAmount = currentRental.amountFor();
             // add frequent renter points
             frequentRenterPoints ++;
             // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) 
+            if ((currentRental.getMovie().getPriceCode() == Movie.NEW_RELEASE) && currentRental.getDaysRented() > 1) 
                 frequentRenterPoints ++;
             //show figures for this rental
-            statement += "\t" + each.getMovie().getTitle()+ "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(thisAmount) + "\n";
+            statement += "\t" + currentRental.getMovie().getTitle()+ "\t" + "\t" + currentRental.getDaysRented() + "\t" + String.valueOf(thisAmount) + "\n";
             totalSum += thisAmount;
         }
         //add footer lines
